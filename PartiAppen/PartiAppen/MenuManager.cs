67bc23656626150ad98bff5c8c63a7ab293e4419
ShiraZull HP <julius.roolf@gmail.com>
2019-11-13 14:21:46 +0100

    Reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- PartiAppen/PartiAppen/MenuManager.cs
+++ PartiAppen/PartiAppen/MenuManager.cs
@@ -16,11 +16,8 @@ namespace PartiAppen
         {
             Menu,
             Program,
-            Politicians,
-            History,
-            About,
-            Press,
-            Contact
+            OmOss,
+            Press
         }
 
         // Create menu pages
@@ -30,6 +27,7 @@ namespace PartiAppen
 
         public static MouseState MouseState => Mouse.GetState();
         public static Point MousePosition => Mouse.GetState().Position;
+        public static bool mouseNotPressed = true;
 
         public static bool ContainsMouse(Rectangle rectangle)
         {
@@ -42,25 +40,78 @@ namespace PartiAppen
             menuFont = Content.Load<SpriteFont>(@"Fonts/Main");
 
 
-            // Menu
-            menu.Pages[0].AddButtonList_Single(menuFont, new Vector2(60, 240), 100f, new[] { "Vårt program", "Vår historia", "Våra politiker", "Om oss", "Press", "Kontakt" });
+            #region Menu
+            menu.Pages[(int)Menues.Menu].AddButtonList_Single(menuFont, new Vector2(60, 240), 100f, new[] { "Vårt program", "Om oss", "Press" });
+            #endregion
 
-            // Program
+            #region Program
+            menu.Pages[(int)Menues.Program].AddButton_Single(menuFont, new Vector2(0), "Meny");
+            menu.Pages[(int)Menues.Program].AddButtonList_Single(menuFont, new Vector2(180, 360), 100f, new[] { "Vårt program", "Vår historia", "Våra politiker", "Om oss", "Press", "Kontakt" });
+            #endregion
 
-            // Historia
+            #region OmOss
+            menu.Pages[(int)Menues.OmOss].AddButtonList_Single(menuFont, new Vector2(60, 240), 100f, new[] { "Vårt program", "Vår historia", "Våra politiker", "Om oss", "Press", "Kontakt" });
+            #endregion
 
-            // Politiker
-
-            // Om Oss
-
-            // Press
-
-            // Kontakt
+            #region Press
+            menu.Pages[(int)Menues.Press].AddButtonList_Single(menuFont, new Vector2(60, 240), 100f, new[] { "Vårt program", "Vår historia", "Våra politiker", "Om oss", "Press", "Kontakt" });
+            #endregion
         }
 
         public void Update()
         {
+
+
+
+
             menu.Update();
+
+            #region Menu
+
+            if (mouseNotPressed && MouseState.LeftButton == ButtonState.Pressed)
+            {
+
+                // Check if player select first button in main menu (play)
+                if (menu.State(0, 0))
+                {
+                    // Change page to Program screen
+                    menu.PageSelection = 1;
+                }
+
+                // Highscore
+                if (menu.State(0, 1))
+                {
+                    menu.PageSelection = 2;
+                }
+
+                // Options
+                if (menu.State(0, 2))
+                {
+                    menu.PageSelection = 3;
+                }
+
+                // How to play
+                if (menu.State(0, 3))
+                {
+                    menu.PageSelection = 4;
+                }
+
+                // Credits
+                if (menu.State(0, 4))
+                {
+                    menu.PageSelection = 5;
+                }
+                
+
+                // Make single activation press reset
+                mouseNotPressed = false;
+            }
+
+            #endregion
+
+
+            if (MouseState.LeftButton == ButtonState.Pressed)
+                mouseNotPressed = true;
         }
 
         public void Draw(SpriteBatch spriteBatch)
