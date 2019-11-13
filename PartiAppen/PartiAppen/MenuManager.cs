74b1f0fc3835a1347e08d52ab36802f586138ab9
Alexander Öberg <stativetroller7@gmail.com>
2019-11-13 19:34:10 +0100

    mitt commit

--- PartiAppen/PartiAppen/MenuManager.cs
+++ PartiAppen/PartiAppen/MenuManager.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,26 +46,32 @@ namespace PartiAppen
             SpriteFont menuFont = content.Load<SpriteFont>(@"Fonts/Main");
 
             // Color theme
-            Color backColor = Color.LightGray, highLightColor = Color.LightBlue;
+            Color primary = new Color(3,169,244), primaryLight = new Color(103,218,255), primaryDark = new Color(0,122,193);
+            Color backColor = Color.LightGray, highLightColor = primary;
             int headerPadding = 300;
-            Rectangle mainRec = new Rectangle(0, headerPadding, 720, 80);
+            //Rectangle mainRec = new Rectangle(0, 1080 - 80 * (MenuesAmount - 1), 720, 80);
+            Rectangle mainRec = new Rectangle(0, 626, 720, 80);
+            Rectangle logoRect = new Rectangle(720 / 2 - (586 / 2), 20, 586, 586);
             Vector2 padding = new Vector2(-20);
+            Texture2D logo = content.Load<Texture2D>(@"Images/logo");
 
             // BackButton is in all pages but menu
             #region BackButton
 
-            Texture2D backArrow;
+            Texture2D backArrow = content.Load<Texture2D>(@"Images/1x/baseline_arrow_back_black_48dp");
 
-            backArrow = content.Load<Texture2D>(@"Images/1x/baseline_arrow_back_black_48dp");
             for (int i = 1; i < MenuesAmount; i++) // skip first page ( menu )
             {
-                // Icon
+                // Icon TODO: change rectangle
                 menu.Pages[i].AddImageButton(new ImageButton(backArrow, new Rectangle(new Point(20), new Point(80)), () => SetMenuState(Menues.Menu)));
             }
 
             #endregion
 
             #region Menu
+            // Logo
+            menu.Pages[(int)Menues.Menu].AddImage(new Image(logo, new Rectangle(720/2 - (586 / 2), 20, 586,586)));
+            // Buttons
             menu.Pages[(int)Menues.Menu].AddButtonList(menuFont, mainRec, 80f, new[] { "Vårt program", "Om oss", "Press" }, padding, backColor, highLightColor, new Action[] {() => SetMenuState(Menues.Program), () => SetMenuState(Menues.OmOss), () => SetMenuState(Menues.Press)});
             #endregion
 
